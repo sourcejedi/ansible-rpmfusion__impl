76bc6b28912f39fce22ee0c46904533e7107e805
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2024-06-26 16:01:31 +0100

    Switch to gpg --show-keys
    keyfile_to_fingerprints was failing on first run, due to ~/.gnupg
    not having been created yet.  (And due to the *fallback* command not
    working, but creating ~/.gnupg as a side effect).
    
    Just use the new --show-keys command, and remove all these old workarounds.

--- files/keyfile_to_fingerprints.sh
+++ files/keyfile_to_fingerprints.sh
@@ -1,23 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-set -o pipefail
 
-# https://unix.stackexchange.com/questions/335669/gnupg-2-1-16-with-fingerprint-no-longer-works-to-show-fingerprints/393975
-# We should use machine-readable output `--with-colons`,
-# except it doesn't work (see answer in link)
+# In theory, we should use machine-readable output `--with-colons`.
+# This will work OK, until it doesn't.
 
-gpg_show_fingerprints() {
-	RC=0
-	gpg --with-fingerprint --import-options import-show --dry-run --import < "$1" >/dev/null 2>&1 || RC=$?
-	if [ $RC == 2 ]; then
-		# Usage error.  Try the old way.
-		gpg --with-fingerprint "$1"
-	else
-		gpg --with-fingerprint --import-options import-show --dry-run --import < "$1"
-	fi
-}
-
-gpg_show_fingerprints "$1" |
+gpg --show-keys --with-fingerprint "$1" |
 	sed -E -n -e 's/.*(([0-9A-F]{4}[ ]*){10,}).*/\1/ p' |
 	sort
 
